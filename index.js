0652f737ed18d7883394daadfc1bbde41e92f76b
MikeMike39 <Mfonseca294@gmail.com>
2021-03-13 15:56:09 -0500

    Done with assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+
+//Function allowing addition w/returning value
+function add(x, y) {
+    return x + y
+}
+//Function allowing division w/returning value
+function divide(x, y) {
+    return x / y
+}
+//Function allowing multiply w/returning value
+function multiply(x, y) {
+    return x * y
+}
+//Function allowing subtraction w/returning value
+function subtract(x, y) {
+    return x - y
+}
+//Function allowing makeInt to parseInt returns NaN
+function makeInt() {
+    parseInt();
+    return parseInt;
+}
+//Function allowing preserveDecimal to parseFloat returns NaN
+function preserveDecimal() {
+    parseFloat();
+    return parseFloat;
+}
+//Function allowing preserveDecimal to take in a string parseFloat return
+function preserveDecimal() {
+    parseFloat();
+    return parseFloat;
+}
+//Function allowing increments an returns results
+function increment(a) {
+    return a + 1;
+}
+//Function allowing decrements an returns results
+function decrement(a) {
+    return a - 1;
+}
+//Function makeInt parses n as an integer and returns the parsed integer
+function makeInt(n) {
+    parseInt(n);
+    return parseFloat(n);
+
+}
+//Function preserveDecimals parses n as a floating point number and returns the parsed number
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat(n);
+
+}
+//Function makeInt assumes base 10
+//function makeInt(x, base) {
+ //  const parsed = parseInt(x, base);
+  // if (isNaN(parsed)) { return 0; }
+  // return parsed * 0;
+ // }
